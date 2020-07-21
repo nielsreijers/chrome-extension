559385786bc8863422c8cd336bed95804fbec035
Niels Reijers <nielsreijers@gmail.com>
2020-07-21 17:06:32 +0800

    automatically add our icon to new links in conversations when we scroll up (still only <a href>)

--- content.js
+++ content.js
@@ -4,16 +4,26 @@ POPOVER_ID = "VLIEGTUIG_POPOVER"
 POPOVER_CONTENT_ID = "VLIEGTUIG_POPOVER_CONTENT"
 POPOVER_CLOSE_BUTTON_ID = "VLIEGTUIG_POPOVER_CLOSE_BUTTON"
 
-getConversationTexts = function() {
-    return document.querySelectorAll(`.${FB_CLASS_MESSAGE_SPAN} > span`)
+getConversationTexts = function(parent) {
+    if (parent.querySelectorAll == undefined) {
+        // not an html node (probably text)
+        return []
+    } else {
+        return parent.querySelectorAll(`.${FB_CLASS_MESSAGE_SPAN} > span`)        
+    }
 }
 
-getConversationLinks = function() {
-    return document.querySelectorAll(`.${FB_CLASS_MESSAGE_SPAN} > span > a`)
+getConversationLinks = function(parent) {
+    if (parent.querySelectorAll == undefined) {
+        // not an html node (probably text)
+        return []
+    } else {
+        return parent.querySelectorAll(`.${FB_CLASS_MESSAGE_SPAN} > span > a`)
+    }
 }
 
-getUnmarkedConversationLinksAndMark = function() {
-    var linkElements = getConversationLinks();
+getUnmarkedConversationLinksAndMark = function(parent) {
+    var linkElements = getConversationLinks(parent);
     var unmarkedElements = Array.prototype.filter.call(linkElements, e => e.getAttribute(MARKED_LINK_ATTRIBUTE) == null);
     unmarkedElements.forEach(e => e.setAttribute(MARKED_LINK_ATTRIBUTE, true));
     return unmarkedElements;
@@ -21,14 +31,12 @@ getUnmarkedConversationLinksAndMark = function() {
 
 popoverPinned = false;
 showPopoverUntilClosed = function() {
-console.log("showPopoverUntilClosed");
     getPopoverElement().style.display = "block";
     popoverPinned = true;
     document.getElementById(POPOVER_CLOSE_BUTTON_ID).innerText = "×";
 
 }
 hidePopover = function() {
-console.log("hidePopover");
     getPopoverElement().style.display = "none";
     popoverPinned = false;
     document.getElementById(POPOVER_CLOSE_BUTTON_ID).innerText = "";
@@ -36,7 +44,6 @@ console.log("hidePopover");
 
 hidePopoverTimer = null;
 showPopoverWhileHoveringWithHalfSecondDelay = function() {
-console.log("showPopoverWhileHoveringWithHalfSecondDelay");
     if (hidePopoverTimer != null) {
         clearTimeout(hidePopoverTimer);
         hidePopoverTimer = null;
@@ -44,20 +51,17 @@ console.log("showPopoverWhileHoveringWithHalfSecondDelay");
     getPopoverElement().style.display = "block";
 }
 hidePopoverAfterHalfSecond = function() {
-console.log("hidePopoverAfterHalfSecond");
-console.log("popoverPinned:" + popoverPinned);
     if (!popoverPinned) {
         hidePopoverTimer = setTimeout(function() {
-            console.log("hidePopoverAfterHalfSecond: TIMEOUT");
             getPopoverElement().style.display = "none";
             hidePopoverTimer = null;
         }, 500);
     }    
 }
 
-markLinks = function () {
+markLinks = function(parent) {
     iconImg = chrome.runtime.getURL('images/check-t.png');
-    getUnmarkedConversationLinksAndMark().forEach(a => {
+    getUnmarkedConversationLinksAndMark(parent).forEach(a => {
         var elem = document.createElement("img");
         elem.setAttribute("src", iconImg);
         elem.setAttribute("height", "24");
@@ -66,7 +70,9 @@ markLinks = function () {
         elem.onclick = showPopoverUntilClosed;
         elem.onmouseenter = showPopoverWhileHoveringWithHalfSecondDelay;
         elem.onmouseleave = hidePopoverAfterHalfSecond;
+        // TODO: placement needs some tweaking
         a.parentElement.appendChild(elem);
+        console.log("added icon to " + a.href);
     });
 }
 
@@ -97,15 +103,25 @@ getPopoverElement = function() {
     return popover;
 }
 
+// Listener to communicate with extension popup
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action == "getTexts") {
-        sendResponse(Array.prototype.map.call(getConversationTexts()), x => x.innerText);
+        sendResponse(Array.prototype.map.call(getConversationTexts(document)), x => x.innerText);
     } else if (request.action == "getLinks") {
-        sendResponse(Array.prototype.map.call(getConversationLinks(), x => x.href));
+        sendResponse(Array.prototype.map.call(getConversationLinks(document), x => x.href));
     } else if (request.action == "markLinks") {
-        markLinks();
+        markLinks(document);
     } else {
         sendResponse({}); // Send nothing..
     }
 });
 
+// Observer to monitor DOM changes and add our icon to any links found in conversations
+let domObserver = new MutationObserver(mutations => {
+    for(let mutation of mutations) {
+        for(let addedNode of mutation.addedNodes) {
+            markLinks(addedNode);
+        }
+    }
+});
+domObserver.observe(document, { childList: true, subtree: true });
