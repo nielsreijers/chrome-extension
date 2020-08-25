bbfa43e5333a222f2426156032323080f1e63675
Niels Reijers <nielsreijers@gmail.com>
2020-08-25 18:22:01 +0800

    Add widgets for message boxes in Facebook's new feed UI, but can't auto-reply because we can't find the user id.

--- content-handler-fb-new-feed-messagebox.js
+++ content-handler-fb-new-feed-messagebox.js
@@ -5,16 +5,16 @@ facebookNewFeedMessageboxHandler = {
                 // not an html node (probably text)
                 return []
             } else {
-                let FB_QUERY_MESSAGE = "._5yl5 > span";
-                let FB_QUERY_MESSAGE_LINK = "._5yl5 > span > a";
+                let FB_CLASS_MESSAGE = "h9e7qa53";
+                let FB_QUERY_MESSAGE = ".h9e7qa53 > span:nth-child(2)";
+                let FB_QUERY_MESSAGE_LINK = ".h9e7qa53 > span:nth-child(2) a";
 
                 let textElements = checkMessageText() ? Array.from(addedNode.querySelectorAll(FB_QUERY_MESSAGE)) : [];
                 let linkElements = checkMessageUrls() ? Array.from(addedNode.querySelectorAll(FB_QUERY_MESSAGE_LINK)) : [];
 
                 // A message may or may not include a link.
                 // If they do we only want to keep the link, and discard the surrounding element.
-                let filteredElements = filterElementsWithLinks(textElements, linkElements, t => t, l => l.parentElement.parentElement);
-
+                let filteredElements = filterElementsWithLinks(textElements, linkElements, t => findParentElementWithClass(t, FB_CLASS_MESSAGE), l => findParentElementWithClass(l, FB_CLASS_MESSAGE));
 
                 // In addition, a link with a preview picture may appear in the messages.
                 // This is a <a> link already with recognisable class.
@@ -27,16 +27,8 @@ facebookNewFeedMessageboxHandler = {
     elementToWidgetData:
         function (e) {
             var replyToType = null;
-            var replyToId = _findFantaTab('user', e);
-            if (replyToId!=null) {
-                replyToType = 'user';
-            } else {
-                replyToId = _findFantaTab('thread', e);
-                if (replyToId!=null) {
-                    replyToType = 'group';
-                }
-            }
-            // The element can be either a <a> link, or text in a <span>
+            var replyToId = null;
+            // The element can be either a <a> link, or text in a <div>
             if (e.tagName=="A") {                
                 var content = stripFacebookExtras(e.href);
                 var contentType = contentTypes.URL;
@@ -57,39 +49,16 @@ facebookNewFeedMessageboxHandler = {
         },
     addWidgetToElement:
         function (tag, e) {
-            let FB_CLASS_TO_APPEND_ICON_PARENT = "_5wd4";
-            let FB_QUERY_TO_APPEND_ICON_CHILD = "._2u_d";
-
-            // Search up to find the top level of this message
-            var parent = findParentElementWithClass(e, FB_CLASS_TO_APPEND_ICON_PARENT);
-
-            // Then there should be a child with this class,
-            // which contains the more (three dots), and forward buttons.
-            var child = null;
-            if (parent != null) {
-                if ((child = parent.querySelector(FB_QUERY_TO_APPEND_ICON_CHILD)) != null) {
+            let FB_CLASS_MESSAGE = "h9e7qa53";
+            let message = findParentElementWithClass(e, FB_CLASS_MESSAGE);
+            if (message != null && message.nextElementSibling != null) {
+                if (message.nextElementSibling.querySelector('.vliegtuig-widget-div') == null) {
                     // Don't add a widget if it's already there. This happens for links that
                     // show both the url as a text, and the box with a preview and title.
-                    if (child.querySelector(".vliegtuig-widget-div") == null) {
-                        child.prepend(tag);
-                    }
+                    message.nextElementSibling.prepend(tag);
                 }
+            } else {
+                e.prepend(tag);
             }
         }
 };
-
-function _findFantaTab(tabType, messageElement) {
-    let re = new RegExp(`fantaTabMain-${tabType}:([0-9]+)`);
-    var e = messageElement;
-
-    fantaTabMain = findParentElementWithClass(messageElement, 'fantaTabMain-')
-    if (fantaTabMain != null) {
-        let c = fantaTabMain.getAttribute("class");
-        let match = re.exec(c);
-        if (match != null) {
-            return match[1];
-        }
-    }
-    return null;    
-}
-
